78eda6cb930859ea264843ea5c98cb8f06de167f
John Clegg <john@projectx.co.nz>
2016-05-13 09:30:57 +1200

    remove the merge issues on truck.go

--- api-go/truck.go
+++ api-go/truck.go
@@ -5,9 +5,6 @@ type Truck struct {
   Name string
   Latitude float64 `db:"lat"`
   Longitude float64 `db:"lng"`
-<<<<<<< HEAD
   Url string
   IsOpen bool `db:"is_open"`
-=======
->>>>>>> 23d792e7480161b721d0600a261d8c3dba84d9cb
 }
